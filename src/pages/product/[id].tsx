969d5042638fe794d40e54a46c328aca1b8da742
danielpvpao <danielpvpao@gmail.com>
2024-11-22 23:11:38 -0300

    New Shopping Cart feature and changes to the layout.

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,90 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "../product"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { stripe } from "@/src/lib/stripe"
+import Stripe from "stripe"
+import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+import Head from "next/head"
+interface ProductProps {
+    product: {
+        id: string,
+    name: string,
+    imageUrl: string,
+    price: number;
+    description: string;
+    defaultPriceId: string;
+    }
+}
+export default function Product({product}: ProductProps){
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+    async function  handleBuyProduct() {
+    try{
+        setIsCreatingCheckoutSession(true)
+        const response = await axios.post('/api/checkout', {
+            priceId: product.defaultPriceId,
+        })
+        const { checkoutUrl } = response.data;
+
+        window.location.href = checkoutUrl
+    }   catch (err) {
+        setIsCreatingCheckoutSession(false)
+        alert('Falha ao redirecionar ao checkout!')
+    }
+    }
+return(
+    <>
+    <Head>
+        <title>{product.name} | Ignite Shop</title>
+    </Head>
+
+
+    <ProductContainer>
+        <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={400} alt=""/>
+        </ImageContainer>
+        <ProductDetails>
+            <h1>{product.name}</h1>
+            <span>{product.price}</span>
+            <p>{product.description}</p>
+            <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+                Comprar agora
+            </button>
+        </ProductDetails>
+    </ProductContainer>
+    </>
+)
+}
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+            paths: [
+                { params: {id: 'prod_'}}
+            ],
+            fallback: "blocking",
+        }
+}
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price']
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: { 
+            product: {
+            id: product.id,
+            name: product.name,
+            imageUrl: product.images[0],
+            price: new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(price.unit_amount / 100),
+            description: product.description,
+            defaultPriceId: price.id,
+        }},
+       revalidate: 60 * 60 * 1, 
+    }
+}
