0aa823179216d792711a976c9318069985ec06de
danielpvpao <danielpvpao@gmail.com>
2024-11-23 10:07:19 -0300

    Test to post on Vercel

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,8 +2,8 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: '2024-10-28.acacia',
+    typescript: true,
     appInfo: {
         name: 'Ignite Shop',
-        version: '1.0.0',
     },
 });
